576eed114531c67662235d42498409391e3002cf
shaynlink <guemmoudrayane03@gmail.com>
2024-02-05 14:45:00 +0100

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom';
-import router from './App';
+import { RouterProvider } from 'react-router-dom'
+import router from './Router'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-
-);
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
