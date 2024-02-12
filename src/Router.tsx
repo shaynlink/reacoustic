ecf5b591abfb64d8329ddd6cb139239861253d67
AMORAND <adrien.morand@ynov.com>
2024-02-12 13:56:07 +0100

    end-game page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/home/home'
+import EndGame from './pages/end-game/end-game'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/end-game',
+    element: <EndGame />
   }
 ])
 
