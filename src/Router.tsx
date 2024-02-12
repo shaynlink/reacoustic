5ece7788a820ee2668495b29af6517ed5b837dad
AMORAND <adrien.morand@ynov.com>
2024-02-12 11:51:40 +0100

    start room page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/home/home'
+import Room from './pages/room/room'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/room',
+    element: <Room />
   }
 ])
 
